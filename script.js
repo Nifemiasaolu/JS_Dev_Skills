8a43837165b2b3cd169504119ae0f2306b59a251
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-04-26 16:48:40 +0100

    Solving new task

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const giveUs = '23';
+// HOW TO SOLVE PROBLEMS
 
-const calcAge = (birthYear) => 2037 - birthYear;
+// PROBLEM
+//  We work for a company building a smart home thermometer.O
+//  Our most recent task is this: "Given an array of temperatures
+// amplitude of one day, calculate the temperature amplitude.
+// Keep in mind that sometimes there might be a sensor error."
 
-console.log(1991);
+const temperature = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
-const calcAverage = (a, b, c) => a+b+c;
+// 1) Understanding the problem
+// - What is temp amplitude? Answer: difference between
+// highest and lowest temp.
+// - How to compute max and min temperatures?
+// - What's a sensor error? And what to do?
 
-console.log(calcAverage(1, 2, 3) );
+// 2) Breaking up into sub-problems
+// - How to ignore errors?
+// - Find max value in temp array
+// - Find min value in temp array
+// - Subtract min from max (amplitude) and rturn it.
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+    console.log(max, min);
+    // return max - min;
+  }
+};
+const amplitude = calcTempAmplitude(temperature);
+
+// PROBLEM 2
+
+// Function should now receive 2 arrays of temps 
+
+// 1) Understanding the problem 
+// - With 2 arrays. should we implement functionality twice?
+// NO! Just merge two arrays 
+
+// 2) Breaking up into sub-problems 
