e393de77f31dcdc41e3b49d69b5c610c49e07d59
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-04-27 17:22:06 +0100

    debug commit

--- script.js
+++ script.js
@@ -69,49 +69,49 @@
 // const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 // console.log(amplitudeNew);
 
-
-
 ///////////////////
 // DEBUGGING (FIXING ERRORS)
 
-const measureKelvin = function () {
-  const measurement = {
-    type: 'temp',
-    unit: 'celsius',
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: 'temp',
+//     unit: 'celsius',
 
-    // C) FIX THE BUG 
+    // C) FIX THE BUG
     // value: Number(prompt('Degrees celsius')),
-    value: 10
-  }
-  
-  // B) FIND THE BUG 
-  console.table(measurement);
+  //   value: 10,
+  // };
 
-  const kelvin = measurement.value + 273;
-  return kelvin;
-}
+  // B) FIND THE BUG
+//   console.table(measurement);
 
-// A) IDENTIFY THE BUG 
-console.log(measureKelvin());
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+
+// A) IDENTIFY THE BUG
+// console.log(measureKelvin());
+
+// Using a Debugger
+
+// const calcTempAmplitudeBug = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+
+//   let max = 0;
+//   let min = 0;
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// const amplitudeBug = calcTempAmplitudeBug([4, 5, 1], [9, 4, 5]);
+// console.log(amplitudeBug);
 
 
-// Using a Debugger 
-const calcTempAmplitudeBug = function (t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
-
-  let max = 0;
-  let min = 0;
-
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
-
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(max, min);
-  return max - min;
-}
-const amplitudeBug = calcTempAmplitudeBug([4, 5, 1], [9, 4, 5]);
-console.log(amplitudeBug);
