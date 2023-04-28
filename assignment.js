93c9171239680148bf31c54b78b92f75876be922
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-04-28 18:01:01 +0100

    Code challenge

--- assignment.js
+++ assignment.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const measureKelvin = function () {
+      const measurement = {
+        type: 'temp',
+        unit: 'celsius',
+    
+        C) FIX THE BUG
+        value: Number(prompt('Degrees celsius')),
+        value: 10,
+      };
+    
+      B) FIND THE BUG
+      console.table(measurement);
+    
+      const kelvin = measurement.value + 273;
+      return kelvin;
+    };
+    
+    A) IDENTIFY THE BUG
+    console.log(measureKelvin());
